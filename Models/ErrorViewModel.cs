844ddd1200387fe9bca4594e9b19876ea633b58c
Nine van de Sande <110717@st-maartenscollege.nl>
2023-04-12 11:47:34 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eindproject_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
